5ee16982929dfd42aac6e3f64a2da052856a2abe
TronSmartContract <48501897+TronSmartContract@users.noreply.github.com>
2019-06-24 19:31:45 +0800

    update

--- list.js
+++ list.js
@@ -1,17 +1,19 @@
 var tronSoljsonSources = [
-  "tron_0.4.25_Odyssey_v3.2.3.js",
-  "tronbox_soljson_v3.js",
-  "tronbox_soljson_v2.js",
-  "tronbox_soljson_v1.js",
+  'soljson-0.5.4_Odyssey_v3.6.0',
+  'tron_0.4.25_Odyssey_v3.2.3.js',
+  'tronbox_soljson_v3.js',
+  'tronbox_soljson_v2.js',
+  'tronbox_soljson_v1.js'
 ];
 var tronSoljsonReleases = {
-  "Tron:0.4.25_Odyssey_v3.2.3": "tron_0.4.25_Odyssey_v3.2.3.js",
-  "Tronbox:SolcjsV3": "tronbox_soljson_v3.js",
-  "Tronbox:SolcjsV2": "tronbox_soljson_v2.js",
-  "Tronbox:SolcjsV1": "tronbox_soljson_v1.js",
+  'Tron:0.5.4_Odyssey_v3.6.0': 'soljson-0.5.4_Odyssey_v3.6.0',
+  'Tron:0.4.25_Odyssey_v3.2.3': 'tron_0.4.25_Odyssey_v3.2.3.js',
+  'Tronbox:SolcjsV3': 'tronbox_soljson_v3.js',
+  'Tronbox:SolcjsV2': 'tronbox_soljson_v2.js',
+  'Tronbox:SolcjsV1': 'tronbox_soljson_v1.js'
 };
 
-if (typeof(module) !== 'undefined')
+if (typeof module !== 'undefined')
   module.exports = {
     'allVersions': tronSoljsonSources,
     'releases': tronSoljsonReleases
