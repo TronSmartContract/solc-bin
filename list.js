5a04cff0fa643f5dd9e22148d10ec6bcddc3b9a9
TronSmartContract <48501897+TronSmartContract@users.noreply.github.com>
2019-06-25 15:23:38 +0800

    update

--- list.js
+++ list.js
@@ -1,12 +1,12 @@
 var tronSoljsonSources = [
-  'soljson-0.5.4_Odyssey_v3.6.0.js',
+  'tron_0.5.4_Odyssey_v3.6.0.js',
   'tron_0.4.25_Odyssey_v3.2.3.js',
   'tronbox_soljson_v3.js',
   'tronbox_soljson_v2.js',
   'tronbox_soljson_v1.js'
 ];
 var tronSoljsonReleases = {
-  'Tron:0.5.4_Odyssey_v3.6.0': 'soljson-0.5.4_Odyssey_v3.6.0.js',
+  'Tron:0.5.4_Odyssey_v3.6.0': 'tron_0.5.4_Odyssey_v3.6.0.js',
   'Tron:0.4.25_Odyssey_v3.2.3': 'tron_0.4.25_Odyssey_v3.2.3.js',
   'Tronbox:SolcjsV3': 'tronbox_soljson_v3.js',
   'Tronbox:SolcjsV2': 'tronbox_soljson_v2.js',
